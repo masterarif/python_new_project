7e68e1e0b17cd27fffcf82131bf8bb082c7d7307
masterarif <marifk2000@googlemail.com>
2020-11-12 10:53:46 +0000

    comment

---
+++ array_py.py
@@ -0,0 +1,34 @@
+colours = ["voilet", "indigo", "blue", "green", "yellow", "orange", "red"]
+print(colours)
+colours.append("Brown")
+
+print(colours)
+
+del colours[4]
+print(colours)
+
+colours.remove("blue")
+print(colours)
+
+colours.pop(3)
+print(colours)
+
+concat = [1, 2, 3]
+print(concat)
+
+concat = concat + [4, 5, 6]
+print(concat)
+
+repeat = ['a']
+print(repeat)
+repeat = repeat * 5
+print(repeat)
+
+#slicing an array
+fruits = ['Apple', 'Banana', 'Mango', 'Grapes', 'Orange']
+print(fruits)
+print(fruits[1:4])
+print(fruits[:3])
+print(fruits[-4:])
+print(fruits[-3:-1])
+
--- class-py.py
+++ class-py.py
@@ -13,3 +13,18 @@ cmplx1 = MyComplexNumber(40,50)
 # calling displayComplex() function
 # Output: 40 + 50j
 cmplx1.displayComplex()
+
+# create another objects against MyComplexNumber class
+# and create a new attribute 'new_attribute'
+cmplx2 = MyComplexNumber(60,70)
+cmplx2.new_attribute = 80
+print((cmplx2.real_part, cmplx2.imag_part, cmplx2.new_attribute))
+
+# Deleting object attributes and the object
+print(cmplx1)
+del cmplx1.real_part
+del cmplx1
+
+# print(cmplx1)
+#Error object deleted
+
